b710ccb38d9c2516195a9a9289820378bf4ea505
Mathematician-Roy <mark8801011@gmail.com>
2020-06-27 15:39:18 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -3,85 +3,14 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 
 func main() {
-	// rand.Seed(time.Now().UnixNano())
-	//fmt.Println("Hello World", "OPOPO")
-	//fmt.Println(rand.Intn(100))
 
-	// Reel Strips
-	reel1 :=[] int {5, 5, 5, 1, 100, 101}
-	reel2 :=[] int {5, 5, 5}
-	reel3 :=[] int {5, 3, 5}
-	reel4 :=[] int {5, 5, 5, 1, 1, 30, 36, 31}
-	reel5 :=[] int {5, 3, 5, 3, 1, 1, 3, 42, 44}
-
-	instanceReel := new(Reel)
-	instanceReel.Init(reel1, reel2, reel3, reel4, reel5)
-	// fmt.Println(instanceReel.reelLength)
-
-	// fmt.Println("Here", index(instanceReel))
-
-	result := spin(instanceReel)
-	fmt.Println(result, result[0][1])
-
-	// Count
-	// payTable, payLine
-	payTable := map[int][]int{
-		0: {0, 0, 0, 0, 0, 0},
-		1: {0, 0, 0, 0, 0, 0},
-		2: {0, 0, 0, 0, 0, 0},
-		3: {0, 0, 0, 0, 0, 0},
-		4: {0, 0, 0, 0, 0, 0},
-		5: {0, 0, 0, 35, 70, 350},
-		6: {0, 0, 0, 30, 60, 300},
-		7: {0, 0, 0, 25, 50, 250},
-		8: {0, 0, 0, 20, 40, 200},
-		9: {0, 0, 0, 15, 30, 150},
-		10: {0, 0, 0, 12, 24, 120},
-		11: {0, 0, 0, 10, 20, 100},
-		12: {0, 0, 0, 5, 10, 50},
-		13: {0, 0, 0, 3, 6, 30}}
-
-	payLine := [][]int{
-			{1, 1, 1, 1, 1},
-			{0, 0, 0, 0, 0},
-			{2, 2, 2, 2, 2},
-			{0, 1, 2, 1, 0},
-			{2, 1, 0, 1, 2},
-			{0, 1, 1, 1, 0},
-			{2, 1, 1, 1, 2},
-			{1, 0, 0, 0, 1},
-			{1, 2, 2, 2, 1},
-			{0, 0, 1, 2, 2},
-			{2, 2, 1, 0, 0},
-			{1, 0, 1, 2, 1},
-			{1, 2, 1, 0, 1},
-			{0, 1, 0, 1, 0},
-			{2, 1, 2, 1, 2},
-			{1, 2, 0, 2, 1},
-			{1, 0, 2, 0, 1},
-			{1, 1, 0, 1, 1},
-			{1, 1, 2, 1, 1},
-			{0, 0, 2, 0, 0},
-			{2, 2, 0, 2, 2},
-			{0, 1, 0, 1, 2},
-			{2, 1, 2, 1, 0},
-			{1, 0, 0, 1, 2},
-			{1, 2, 2, 1, 0}}
-
-	// fmt.Println(payTable[0], payLine[0])
-	// fmt.Println(payLine[0])
-	// fmt.Println(getSymbol(result, payLine, 2))
-	// fmt.Println(findKeyConnection(result, payLine, 0, 3))
-	// test := findKeyConnection(result, payLine, 0, 3)
-	// fmt.Println(test)
-	// fmt.Println(prizeMapping_single(test[0], test[1], payTable))
-	// fmt.Println(prizeMapping(result, 25, payLine, payTable))
-	RTP := simulator_mainGame(instanceReel, 25, payLine, payTable, 10000000)
-	fmt.Println(RTP)
+	simulator_mainGame_goroutine(10000000)
+	// 0.61188126
 }
 
 type Reel struct {
@@ -241,6 +170,7 @@ func prizeMapping_single(connectSymbol int, connectLine int, paytable  map[int][
 	}else{
 
 		prize = paytable[connectSymbol][connectLine]
+		// fmt.Println(prize)
 	}
 
 	return prize
@@ -258,12 +188,136 @@ func prizeMapping(reelList [][]int, payline int, payLine [][]int, paytable  map[
 	return totalPrize
 }
 
-func simulator_mainGame(reel *Reel, payline int, payLine [][]int, paytable  map[int][]int, t int) int{
+func simulator_mainGame(reel *Reel, payline int, payLine [][]int, paytable  map[int][]int, t int) float32{
 	sum := 0
 	for i := 0; i < t; i++{
 		reelSpin := spin(reel)
 		totalOdds := prizeMapping(reelSpin, payline, payLine, paytable)
 		sum += totalOdds
 	}
-	return sum / (payline * t)
+
+	return float32(sum)  / float32(payline * t)
+}
+
+func simulator_mainGame_goroutine(times int){
+	rand.Seed(time.Now().UnixNano())
+	//fmt.Println("Hello World", "OPOPO")
+	//fmt.Println(rand.Intn(100))
+
+	// Reel Strips
+	reel1 :=[] int {7, 2, 12, 10, 13, 9, 7, 12, 13, 10, 2, 6, 12, 10, 13, 12, 11, 12, 7, 12, 13, 5, 5, 5, 9, 10, 7, 13, 11, 10, 12, 7, 12, 5, 8, 10, 12, 9, 10, 7, 9, 12, 10, 13, 12, 6, 9, 10, 7, 7, 8, 9, 10, 6, 11, 12, 10, 7, 8, 12, 11, 13, 12, 8, 13, 7}
+	reel2 :=[] int {7, 2, 12, 10, 13, 9, 12, 12, 13, 10, 2, 6, 8, 13, 12, 3, 11, 12, 6, 12, 13, 12, 5, 12, 9, 3, 6, 11, 12, 10, 5, 12, 5, 6, 3, 10, 9, 13, 6, 10, 11, 3, 8, 13, 12, 7, 12, 10, 12, 10, 8, 12, 9, 12, 3, 13, 8, 13, 10, 13, 12, 9, 13}
+	reel3 :=[] int {7, 2, 12, 10, 8, 9, 10, 12, 13, 10, 2, 6, 8, 12, 12, 3, 11, 12, 6, 12, 13, 5, 5, 5, 9, 3, 6, 11, 12, 10, 5, 12, 13, 13, 3, 12, 9, 9, 6, 10, 10, 3, 8, 13, 12, 7, 12, 10, 12, 10, 8, 9, 12, 7, 3, 12, 10, 13, 8, 12, 10, 13, 8}
+	reel4 :=[] int {7, 2, 12, 10, 8, 9, 10, 12, 13, 10, 2, 6, 8, 12, 12, 3, 11, 12, 6, 8, 13, 12, 5, 5, 9, 3, 6, 11, 12, 10, 5, 12, 11, 13, 3, 12, 9, 9, 6, 10, 10, 3, 12, 13, 8, 7, 12, 10, 12, 10, 8, 9, 12, 7, 3, 12, 10, 13, 8, 12, 10, 13, 8}
+	reel5 :=[] int {7, 2, 12, 10, 8, 9, 10, 12, 13, 10, 2, 6, 8, 12, 12, 3, 11, 6, 6, 8, 13, 2, 5, 5, 9, 11, 6, 11, 12, 10, 5, 13, 11, 7, 11, 10, 9, 9, 6, 11, 10, 11, 8, 13, 8, 7, 9, 10, 12, 10, 8, 9, 12, 7, 11, 8, 10, 8, 9, 12, 8, 6, 10, 9, 7}
+
+	instanceReel := new(Reel)
+	instanceReel.Init(reel1, reel2, reel3, reel4, reel5)
+	// fmt.Println(instanceReel.reelLength)
+
+	// fmt.Println("Here", index(instanceReel))
+
+	// result := spin(instanceReel)
+	// fmt.Println(result, result[0][1])
+
+	// Count
+	// payTable, payLine
+	payTable := map[int][]int{
+		0: {0, 0, 0, 0, 0, 0},
+		1: {0, 0, 0, 0, 0, 0},
+		2: {0, 0, 0, 0, 0, 0},
+		3: {0, 0, 0, 0, 0, 0},
+		4: {0, 0, 0, 0, 0, 0},
+		5: {0, 0, 0, 35, 70, 350},
+		6: {0, 0, 0, 30, 60, 300},
+		7: {0, 0, 0, 25, 50, 250},
+		8: {0, 0, 0, 20, 40, 200},
+		9: {0, 0, 0, 15, 30, 150},
+		10: {0, 0, 0, 12, 24, 120},
+		11: {0, 0, 0, 10, 20, 100},
+		12: {0, 0, 0, 5, 10, 50},
+		13: {0, 0, 0, 3, 6, 30}}
+
+	payLine := [][]int{
+		{1, 1, 1, 1, 1},
+		{0, 0, 0, 0, 0},
+		{2, 2, 2, 2, 2},
+		{0, 1, 2, 1, 0},
+		{2, 1, 0, 1, 2},
+		{0, 1, 1, 1, 0},
+		{2, 1, 1, 1, 2},
+		{1, 0, 0, 0, 1},
+		{1, 2, 2, 2, 1},
+		{0, 0, 1, 2, 2},
+		{2, 2, 1, 0, 0},
+		{1, 0, 1, 2, 1},
+		{1, 2, 1, 0, 1},
+		{0, 1, 0, 1, 0},
+		{2, 1, 2, 1, 2},
+		{1, 2, 0, 2, 1},
+		{1, 0, 2, 0, 1},
+		{1, 1, 0, 1, 1},
+		{1, 1, 2, 1, 1},
+		{0, 0, 2, 0, 0},
+		{2, 2, 0, 2, 2},
+		{0, 1, 0, 1, 2},
+		{2, 1, 2, 1, 0},
+		{1, 0, 0, 1, 2},
+		{1, 2, 2, 1, 0}}
+
+	c1 := make(chan float32, 1)
+	c2 := make(chan float32, 1)
+	c3 := make(chan float32, 1)
+	c4 := make(chan float32, 1)
+
+	go func (reel *Reel, payline int, payLine [][]int, paytable  map[int][]int, t int) {
+		sum := 0
+		for i := 0; i < t; i++{
+			reelSpin := spin(reel)
+			totalOdds := prizeMapping(reelSpin, payline, payLine, paytable)
+			sum += totalOdds
+		}
+
+		c1 <- float32(sum)
+	}(instanceReel, 25, payLine, payTable, times/4)
+	go func (reel *Reel, payline int, payLine [][]int, paytable  map[int][]int, t int) {
+		sum := 0
+		for i := 0; i < t; i++{
+			reelSpin := spin(reel)
+			totalOdds := prizeMapping(reelSpin, payline, payLine, paytable)
+			sum += totalOdds
+		}
+
+		c2 <- float32(sum)
+	}(instanceReel, 25, payLine, payTable, times/4)
+	go func (reel *Reel, payline int, payLine [][]int, paytable  map[int][]int, t int) {
+		sum := 0
+		for i := 0; i < t; i++{
+			reelSpin := spin(reel)
+			totalOdds := prizeMapping(reelSpin, payline, payLine, paytable)
+			sum += totalOdds
+		}
+
+		c3 <- float32(sum)
+	}(instanceReel, 25, payLine, payTable, times/4)
+	go func (reel *Reel, payline int, payLine [][]int, paytable  map[int][]int, t int) {
+		sum := 0
+		for i := 0; i < t; i++{
+			reelSpin := spin(reel)
+			totalOdds := prizeMapping(reelSpin, payline, payLine, paytable)
+			sum += totalOdds
+		}
+
+		c4 <- float32(sum)
+	}(instanceReel, 25, payLine, payTable, times/4)
+
+	x1 := <-  c1
+	x2 := <-  c2
+	x3 := <-  c3
+	x4 := <-  c4
+
+	rtp := (x1+x2+x3+x4)/(float32(times)*25)
+
+	fmt.Println(rtp)
+
 }
